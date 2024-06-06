08edfd6bcb124f71563eab1e707794e9e9098074
Anja <ilg@campus.tu-berlin.de>
2024-06-06 11:14:42 +0200

    added a docstring

--- src/crossover.jl
+++ src/crossover.jl
@@ -1,3 +1,8 @@
+"""
+    single_point_crossover(p1, p2)
+
+A single crossover point is selected. Both populant's (`p1` and `p1`) genes will be swapped after that point.
+"""
 function single_point_crossover(p1::ContinuousPopulant,p2::ContinuousPopulant)
     i = rand(1:length(p1.genes))
     return ContinuousPopulant(vcat(p1.genes[begin:i],p2.genes[i:end])),ContinuousPopulant(vcat(p2.genes[begin:i],p1.genes[i:end]))
