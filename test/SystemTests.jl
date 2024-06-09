2ee66a6faf657612fe9b712e309e223b95f82a05
n1colas <nicolaslook@gmx.de>
2024-06-09 15:23:29 +0200

    fix tol in test

--- test/SystemTests.jl
+++ test/SystemTests.jl
@@ -6,5 +6,5 @@ using Julia4ML_GA
     # rosenbrock funktion for a = 1 and b = 100
     # global minimum (x,y) at (a,a**2)
     # solution is (1,1)
-    @test isapprox( Julia4ML_GA.optimize(Float64[0.,0.],x->(1-x[1])^2 +100*(x[2]-x[1]^2)^2,Julia4ML_GA.GeneticAlgorithm();iterations=100), [1.,1.], atol=0.05)
+    @test isapprox( Julia4ML_GA.optimize(Float64[0.,0.],x->(1-x[1])^2 +100*(x[2]-x[1]^2)^2,Julia4ML_GA.GeneticAlgorithm();iterations=100), [[1.,1.]], atol=0.1)
 end
