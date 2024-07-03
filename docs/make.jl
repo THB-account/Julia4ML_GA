6f45eb7abdb4df6230ca2d48ffd492f547b1ce0b
anjailg <49808207+anjailg@users.noreply.github.com>
2024-07-03 16:08:44 +0200

    Update make.jl
    exchanged branch for link

--- docs/make.jl
+++ docs/make.jl
@@ -9,7 +9,7 @@ makedocs(;
     sitename="Julia4ML_GA.jl",
     format=Documenter.HTML(;
         canonical="https://THB-account.github.io/Julia4ML_GA.jl",
-        edit_link="documentation",
+        edit_link="main",
         assets=String[],
     ),
     pages=[
